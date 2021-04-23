5b21e4c854b525a2e16b04622f65f650cc3c59a8
Mazdak <mazpar.2000@gmail.com>
2021-04-23 10:25:25 +0430

    fixed minor issues based on IDE.

--- file_rename_unofficial.py
+++ file_rename_unofficial.py
@@ -1,13 +1,13 @@
 import os
 
-#getting the list of the files in a folder
+# getting the list of the files in a folder
 inp = str(input("Enter your directory: \n   "))
-inp = inp.replace('\\','/')
+inp = inp.replace('\\', '/')
 
 lists = os.listdir(inp)
 
 
-#the following function is for better organization
+# the following function is for better organization
 
 def printf(a):
     for i in a:
@@ -23,13 +23,14 @@ for file in lists:
 
 printf(srt)
 
-#now we need to have the name of the episode
+# now we need to have the name of the episode
 print("Enter the name of the episode: ")
-ep_name = str(input("\"Please replace the number of the episode with {b} in order for the program to work properly\"\n"))
+ep_name = str(input("\"Please replace the number of the episode with {b}\n"
+                    " in order for the program to work properly\"\n"))
 
-#the following renames the ep numbers
-#with the 01 format
-#exmple: 04, 05, 10
+# the following renames the ep numbers
+# with the 01 format
+# example: 04, 05, 10
 k = 1
 b = "0"
 for file in srt:
@@ -38,4 +39,4 @@ for file in srt:
         k += 1
     else:
         k += 1
-    os.rename(f"{inp}" + "/" + f"{file}",f"{inp}" + "/" + f"{ep_name}")
+    os.rename(f"{inp}" + "/" + f"{file}", f"{inp}" + "/" + f"{ep_name}")
